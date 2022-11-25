ca1cc59e4cfdbee163900c1f7e10492c5caf7c6c
dw36 <hin7788966558@gmail.com>
2022-11-25 05:49:47 -0500

    o 25 549

--- newDEV/main.tf
+++ newDEV/main.tf
@@ -233,16 +233,17 @@ output "server_ip" {
   value = digitalocean_droplet.dp_name.*.ipv4_address
  }
 
-output "vpc_id" {  
-    description = "ID of project VPC:"  
-    value     = digitalocean_vpc.web_vpc.id
-  }
+# output "vpc_id" {  
+#   description = "ID of project VPC:"  
+#   value     = digitalocean_vpc.web_vpc.id
+#   }
 
-output "lb_url" {
-      description = "URL of load balancer:"
-      value     = "loadbalancer-assign01"}
+# output "lb_url" {
+#     description = "URL of load balancer:"
+#     value     = "loadbalancer-assign01"
+#   }
       
-output "web_server_count" {
-      description = "Number of web servers provisioned"
-      value       = ${count.index}#var.droplet_count
-      }
+# output "web_server_count" {
+#     description = "Number of web servers provisioned"
+#     value       = ${count.index}#var.droplet_count
+#   }
